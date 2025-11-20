b9a1ef41748429a618b903a400469494199765af
Imane <i.elmostaqim.ca@gmail.com>
2025-11-20 09:54:37 -0500

    correction coquille

--- assets/js/views/Donnees.js
+++ assets/js/views/Donnees.js
@@ -16,7 +16,7 @@ class Donnees {
       <section >
         <div class="contenu-sur-image">
           <img src="assets/img/CEP-Knox-Hiberna.jpg" alt="iamge" />
-          <h1>Murales subventionnées de la vile de Montréal</h1>
+          <h1>Murales subventionnées de la ville de Montréal</h1>
         </div>
         <div class="max-w-[1000px] mx-auto p-6">
           <p class="mb-4 text-lg">Depuis plusieurs années, la Ville de Montréal soutient la réalisation de murales extérieures visibles, créatives et liées à leur contexte. Par le biais de divers programmes, unifiés depuis 2016 au sein du Programme d'art mural, la Ville a soutenu la réalisation de centaines de murales. Cet ensemble de données présente les murales financées par la Ville de Montréal depuis 2007.</p>
